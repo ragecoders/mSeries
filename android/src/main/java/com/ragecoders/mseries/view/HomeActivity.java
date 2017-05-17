01e7c1ecbedc11255eb7a8150240999543c8d0f9
Surkhojb <juanjo@ragecoders.com>
2017-05-17 19:32:42 +0200

    Menu texts added in resources

--- android/src/main/java/com/ragecoders/mseries/view/HomeActivity.java
+++ android/src/main/java/com/ragecoders/mseries/view/HomeActivity.java
@@ -88,17 +88,17 @@ public class HomeActivity extends AppCompatActivity
     // Handle navigation view item clicks here.
     int id = item.getItemId();
 
-    if (id == R.id.nav_camera) {
-      // Handle the camera action
-    } else if (id == R.id.nav_gallery) {
+    if (id == R.id.nav_home) {
 
-    } else if (id == R.id.nav_slideshow) {
+    } else if (id == R.id.nav_following) {
 
-    } else if (id == R.id.nav_manage) {
+    } else if (id == R.id.nav_favorites) {
 
-    } else if (id == R.id.nav_share) {
+    } else if (id == R.id.nav_explore) {
 
-    } else if (id == R.id.nav_send) {
+    } else if (id == R.id.nav_configuration) {
+
+    } else if (id == R.id.nav_about) {
 
     }
 
